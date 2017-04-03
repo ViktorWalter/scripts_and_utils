ae024518bd87f1606cd15cf658a77399f8fd0634
Viktor Walter <viktor_walter@outlook.com>
2017-04-03 11:05:32 +0200

    Added screen color inverter script

---
+++ doti3/invertOutputs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+scr=$( xrandr -q | awk '/ connected/ {print $1}' )
+scr="${scr//$'\n'/ }"
+scrlst=($scr)
+brt=$( xrandr --verbose | awk '/Brightness: / {print $2}')
+brtls=($brt)
+brt=${brtls[1]}
+echo $brt
+brt=$((-${brt%.*}))
+echo $brt
+if [ "$brt" -eq 0 ]; then
+  brt=1;
+fi
+if [ -z "$scr" ]; then
+  i3-nagbar -m 'No screen defined' -t warning;
+else
+  for output in ${scr}; do
+    xrandr --output $output --brightness $brt
+  done;
+fi
