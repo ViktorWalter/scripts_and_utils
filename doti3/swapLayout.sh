5a7868169bf25ba3381a68d1e6da4074d66bf0d1
Viktor Walter <viktor_walter@outlook.com>
2017-04-20 19:38:36 +0200

    Updated the screen arranger and vimrc. Also added Vimium++ setup file.

--- doti3/swapLayout.sh
+++ doti3/swapLayout.sh
@@ -5,8 +5,8 @@ scr="${scr//$'\n'/ }"
 scrlst=($scr)
 if [ -z "$scr" ]; then
   i3-nagbar -m 'No screen defined' -t warning;
-elif [ ${#scrlst[@]} -lt 2 ]; then
-  i3-nagbar -m 'No secondary screen defined' -t warning;
+# elif [ ${#scrlst[@]} -lt 2 ]; then
+  # i3-nagbar -m 'No secondary screen defined' -t warning;
 else
   for output in ${scr}; do
     if [[ "$output" == *LVDS* ]]; then
