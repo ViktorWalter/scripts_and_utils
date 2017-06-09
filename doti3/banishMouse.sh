ef40f6714f8d90c98a45e5a8229b3b4e062fe500
Viktor Walter <viktor_walter@outlook.com>
2017-06-09 14:31:03 +0200

    Banishment is now done with the Win+B combo. Banishment is to the corner of the current window instead of the global zero.

--- doti3/banishMouse.sh
+++ doti3/banishMouse.sh
@@ -1,1 +1,6 @@
-xdotool mousemove 0 0
+#!/bin/bash
+#
+coords=$( xwininfo -id $(xdotool getactivewindow) | awk '/Absolute/ {print $4}' )
+coords="${coords//$'\n'/ }"
+coordlst=($coords)
+xdotool mousemove ${coordlst[0]} ${coordlst[1]}
