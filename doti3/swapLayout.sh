f3fe8ec3844d02e43b00ab44cadc8ceb3a5344ab
Viktor Walter <viktor_walter@outlook.com>
2017-08-01 12:08:55 +0200

    Updated monitor layout swapper. Now it supports more than two monitors.

--- doti3/swapLayout.sh
+++ doti3/swapLayout.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-#i3-nagbar -m "window name is [$1]" -t info 
-scr=$( xrandr -q | awk '/ connected/ {print $1}' )
-scr="${scr//$'\n'/ }"
-scrlst=($scr)
+let -a scr
+let -a res
+# let -a wid
+wid=0
+scr=($( xrandr -q | awk '/ connected/ {print $1}' ))
+res=($( xrandr -q | awk '/ connected/ {getline; print $1}' ))
 if [ -z "$scr" ]; then
   i3-nagbar -m 'No screen defined' -t warning;
-# elif [ ${#scrlst[@]} -lt 2 ]; then
-  # i3-nagbar -m 'No secondary screen defined' -t warning;
 else
-  for output in ${scr}; do
-    if [[ "$output" == *LVDS* ]]; then
-      xrandr --output $output --primary --pos 0x0 --rotate normal;
-    else  
-      xrandr --output $output --pos 1600x0 --rotate normal;
+  for i in "${!scr[@]}"; do
+    if [[ "${scr[$i]}" == *LVDS* ]]; then
+      xrandr --output ${scr[$i]} --primary --pos 0x0 --rotate normal
+      wid=$((wid + $( echo ${res[$i]} | awk -F'x' '{print $1}' )));
+    fi
+  done;
+  for i in "${!scr[@]}"; do
+    if [[ "${scr[$i]}" != *LVDS* ]]; then
+      xrandr --output ${scr[$i]} --pos "$wid"x0 --rotate normal
+      wid=$((wid + $( echo ${res[$i]} | awk -F'x' '{print $1}' )));
     fi;
   done;
 fi
