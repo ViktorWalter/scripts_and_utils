15f867480d5e9ebb87726c06aea189255cb95071
Viktor Walter <viktor_walter@outlook.com>
2017-08-04 09:07:02 +0200

    Added functionality for automatic rotation of a scpecific monitor.

---
+++ doti3/rotateScreen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+let -a scr
+let -a rot
+# let -a wid
+wid=0
+scr=($( xrandr -q | awk '/ connected/ {print $1}' ))
+rot=($( xrandr -q --verbose | awk '/ connected/ {print $5}' ))
+if [ -z "$scr" ]; then
+  i3-nagbar -m 'No screen defined' -t warning;
+else
+  for i in "${!scr[@]}"; do
+    if [[ "${scr[$i]}" == *DP* ]]; then
+      if [[ "${rot[$i]}" == normal ]]; then
+        xrandr --output ${scr[$i]} --rotate left;
+      else
+        xrandr --output ${scr[$i]} --rotate normal;
+      fi
+    fi
+  done;
+fi
