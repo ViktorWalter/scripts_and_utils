9d510473dce8ad3bb5bf2c0d779295392d7dcd45
Viktor Walter <viktor_walter@outlook.com>
2017-08-04 09:26:49 +0200

    Edited swapLayout.sh so that the width depends on rotation.

--- doti3/swapLayout.sh
+++ doti3/swapLayout.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 let -a scr
 let -a res
+let -a rot
 # let -a wid
 wid=0
 scr=($( xrandr -q | awk '/ connected/ {print $1}' ))
 res=($( xrandr -q | awk '/ connected/ {getline; print $1}' ))
+rot=($( xrandr -q --verbose | awk '/ connected/ {print $5}' ))
 if [ -z "$scr" ]; then
   i3-nagbar -m 'No screen defined' -t warning;
 else
@@ -16,8 +18,12 @@ else
   done;
   for i in "${!scr[@]}"; do
     if [[ "${scr[$i]}" != *LVDS* ]]; then
-      xrandr --output ${scr[$i]} --pos "$wid"x0 --rotate normal
-      wid=$((wid + $( echo ${res[$i]} | awk -F'x' '{print $1}' )));
+      xrandr --output ${scr[$i]} --pos "$wid"x0
+      if [[ "${rot[$i]}" == normal ]]; then
+        wid=$((wid + $( echo ${res[$i]} | awk -F'x' '{print $1}' )));
+      else
+        wid=$((wid + $( echo ${res[$i]} | awk -F'x' '{print $2}' )));
+      fi
     fi;
   done;
 fi
