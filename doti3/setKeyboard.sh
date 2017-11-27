6843f8ae1859ed7a5ece721681e6ebce4e53c7c3
Viktor Walter <viktor_walter@outlook.com>
2017-11-27 01:43:26 +0100

    Update

--- doti3/setKeyboard.sh
+++ doti3/setKeyboard.sh
@@ -3,3 +3,11 @@ sleep 1
 setxkbmap -option "caps:escape"
 sleep 1;
 xmodmap -e "keycode 105 = Menu NoSymbol Control_R"
+# sleep 1;
+# xmodmap -e "keycode 43 = h H Left"
+# sleep 1;
+# xmodmap -e "keycode 44 = j J Down"
+# sleep 1;
+# xmodmap -e "keycode 45 = k K Up"
+# sleep 1;
+# xmodmap -e "keycode 46 = l L Right"
