d40272dfa04368124f9807150263d4eb30b1ecd2
Viktor Walter <viktor_walter@outlook.com>
2017-11-27 14:29:56 +0100

    Update

--- doti3/setKeyboard.sh
+++ doti3/setKeyboard.sh
@@ -1,8 +1,9 @@
-setxkbmap -model pc101 -layout us,sk -variant ,qwerty -option "grp:sclk_toggle"
-sleep 1
-setxkbmap -option "caps:escape"
-sleep 1;
-xmodmap -e "keycode 105 = Menu NoSymbol Control_R"
+xmodmap ~/.Xmodmap
+# setxkbmap -model pc101 -layout us,sk -variant ,qwerty -option "grp:sclk_toggle"
+# sleep 1
+# setxkbmap -option "caps:escape"
+# sleep 1;
+# xmodmap -e "keycode 105 = Menu NoSymbol Control_R"
 # sleep 1;
 # xmodmap -e "keycode 43 = h H Left"
 # sleep 1;
