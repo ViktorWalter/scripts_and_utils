8fd5b63541e7582ca5747d6ae8e63b6dfb37e4c2
Viktor Walter <viktor_walter@outlook.com>
2018-10-10 18:56:45 +0200

    Update

--- doti3/swapLayout.sh
+++ doti3/swapLayout.sh
@@ -24,7 +24,7 @@ else
         xrandr --output ${scr[$i]} --off
       fi;
     done;
-    pacmd set-default-sink `pactl list short sinks | grep 1b | tr '\t' ' ' | cut -d' ' -f2`
+    # pacmd set-default-sink `pactl list short sinks | grep 1b | tr '\t' ' ' | cut -d' ' -f2`
   else
     for i in "${!scr[@]}"; do
       if [[ "${scr[$i]}" != *LVDS* ]]; then
@@ -32,7 +32,7 @@ else
         previousMonitor=${scr[$i]}
       fi;
     done;
-    pacmd set-default-sink `pactl list short sinks | grep hdmi | tr '\t' ' ' | cut -d' ' -f2`
+    # pacmd set-default-sink `pactl list short sinks | grep hdmi | tr '\t' ' ' | cut -d' ' -f2`
   fi
 fi
 xrandr --dpi 100
