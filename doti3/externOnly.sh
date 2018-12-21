d36ddffc0cafd664bb2c5c05519dac541cef9c19
Viktor Walter <viktor_walter@outlook.com>
2018-12-21 14:45:12 +0100

    Made the monitor layout scripts intercompatible and stable.

--- doti3/externOnly.sh
+++ doti3/externOnly.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-xrandr --output DP-0 --mode 1920x1080;
-xrandr --output DP-0 --primary;
-xrandr --output LVDS-1-1 --off;
+scr=($( xrandr -q | awk '/ connected/ {print $1}' ))
+active=($( xrandr | grep -E " connected (primary )?[1-9]+" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/" ))
+echo $active | grep LVDS
+if [ $? -eq 0 ]; then
+  xrandr --output DP-0 --auto --primary
+  xrandr --output LVDS-1-1 --off
+else
+  xrandr --output LVDS-1-1 --auto --primary
+  xrandr --output DP-0 --off
+fi
 ~/.i3/setWallpaper.sh
--- doti3/swapLayout.sh
+++ doti3/swapLayout.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 let -a scr
 let -a res
-let -a rot
+# let -a rot
 # let -a wid
 wid=0
 scr=($( xrandr -q | awk '/ connected/ {print $1}' ))
 res=($( xrandr -q | awk '/ connected/ {getline; print $1}' ))
-rot=($( xrandr -q --verbose | awk '/ connected/ {print $5}' ))
+# rot=($( xrandr -q --verbose | awk '/ connected/ {print $5}' ))
 active=($( xrandr | grep -E " connected (primary )?[1-9]+" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/" ))
 if [ -z "$scr" ]; then
   i3-nagbar -m 'No screen defined' -t warning;
@@ -14,7 +14,7 @@ else
   for i in "${!scr[@]}"; do
     if [[ "${scr[$i]}" == *LVDS* ]]; then
       previousMonitor=${scr[$i]}
-      xrandr --output ${scr[$i]} --primary --pos 0x0 --rotate normal
+      xrandr --output ${scr[$i]} --auto --primary --pos 0x0 --rotate normal 
       wid=$((wid + $( echo ${res[$i]} | awk -F'x' '{print $1}' )));
     fi
   done;
