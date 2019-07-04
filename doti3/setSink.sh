c1a51c8d7f2c0ec59a326db8ae6981bdf8107c50
Viktor Walter <viktor_walter@outlook.com>
2019-07-04 10:03:33 +0200

    Adding title to the menu

--- doti3/setSink.sh
+++ doti3/setSink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sinks=`pactl list short sinks | cut -f1-2`
-selected=`echo "$sinks" | rofi -dmenu`
+selected=`echo "$sinks" | rofi -dmenu -p "Select audio device:"`
 selNum=`echo "$selected" | cut -f1`
 pactl set-default-sink $selNum
 outputs=`pactl list short sink-inputs | cut -f1`
