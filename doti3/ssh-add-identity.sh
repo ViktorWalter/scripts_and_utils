f00459293bd024fd2a3d7607a3e18fe22c2888b3
Viktor Walter <viktor_walter@outlook.com>
2019-07-25 13:54:30 +0200

    Improvements ;-)

--- doti3/ssh-add-identity.sh
+++ doti3/ssh-add-identity.sh
@@ -1,3 +1,4 @@
-#/bin/bash
+#/usr/bin/zsh
+eval "$(ssh-agent)"
 password=`python -c "import keyring,sys; sys.stdout.write(keyring.get_password('git-ssh-key','personal'))"`
 expect -c " spawn ssh-add; expect \"Enter passphrase for \"; send \"$password\r\"; expect \"%\"; return 0"
