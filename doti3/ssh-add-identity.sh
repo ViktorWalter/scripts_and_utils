bb66bfe722736d8460aefdff9a1cc7eed9b11e35
Viktor Walter <viktor_walter@outlook.com>
2019-07-25 13:30:53 +0200

    Fixing bug

--- doti3/ssh-add-identity.sh
+++ doti3/ssh-add-identity.sh
@@ -1,2 +1,3 @@
-passwd = `python -c "import keyring,sys; sys.stdout.write(keyring.get_password('git-ssh-key','personal'))"`
-expect -c " spawn ssh-add; expect \"Enter passphrase for \"; send \"$passwd\r\"; expect \"%\"; return 0"                                ==============
+#/bin/bash
+password=`python -c "import keyring,sys; sys.stdout.write(keyring.get_password('git-ssh-key','personal'))"`
+expect -c " spawn ssh-add; expect \"Enter passphrase for \"; send \"$password\r\"; expect \"%\"; return 0"
