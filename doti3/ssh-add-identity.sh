d51ec95b03a542ce35cd544e5ba0d6f424cb5d96
Viktor Walter <viktor_walter@outlook.com>
2019-07-26 11:21:21 +0200

    Improvement on the ssh autofiller

--- doti3/ssh-add-identity.sh
+++ doti3/ssh-add-identity.sh
@@ -1,4 +1,8 @@
 #/usr/bin/zsh
-eval "$(ssh-agent)"
-password=`python -c "import keyring,sys; sys.stdout.write(keyring.get_password('git-ssh-key','personal'))"`
-expect -c " spawn ssh-add; expect \"Enter passphrase for \"; send \"$password\r\"; expect \"%\"; return 0"
+# /usr/bin/ssh-add -l
+/usr/bin/ssh-add -l > /dev/null
+if [ $? -ne 0 ]; then
+  eval "$(ssh-agent)"
+  password=`python -c "import keyring,sys; sys.stdout.write(keyring.get_password('git-ssh-key','personal'))"`
+  expect -c " spawn ssh-add; expect \"Enter passphrase for \"; send \"$password\r\"; expect \"%\"; return 0"
+fi
