b6a983dbe2cff63dda8a92067c2f80e6c7123a96
Viktor Walter <viktor_walter@outlook.com>
2019-10-10 13:08:23 +0200

    Better exit from music player

--- doti3/playMusic.sh
+++ doti3/playMusic.sh
@@ -1,5 +1,9 @@
+cleanup(){
+  killall cava
+}
+trap cleanup INT
 # urxvt -e cava -p ~/.i3/cavaconfig  &
 tmux split-window -v "TERM=xterm-256color; cava -p ~/.i3/cavaconfig"
 tmux last-pane;
  tmux resize-pane -R;
-mpg123 -ZCvb 1024 ~/music/youtube/*
+mpg123 -ZCvb 1024 ~/music/youtube/*;
--- doti3/playStudyMusic.sh
+++ doti3/playStudyMusic.sh
@@ -1,3 +1,7 @@
+cleanup(){
+  killall cava
+}
+trap cleanup INT
 tmux split-window -v "TERM=xterm-256color; cava -p ~/.i3/cavaconfig"
 tmux last-pane;
  tmux resize-pane -R;
