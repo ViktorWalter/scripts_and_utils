36fe416dfd13075b0f5ba544e335c18d8b910778
ViktorWalter <viktor_walter@outlook.com>
2020-07-25 21:19:16 +0200

    better function of brightness control to acount for the visual non-linearity

--- doti3/thinkpad_power_management/setBrightness.sh
+++ doti3/thinkpad_power_management/setBrightness.sh
@@ -5,13 +5,13 @@ fi
 
 current=`cat /sys/class/backlight/intel_backlight/actual_brightness`
 max=`cat /sys/class/backlight/intel_backlight/max_brightness`
-let step=$current/2
+#let step=$current/2
 case $1 in
   "+")
-    let val=$current+$step
+    let val=$current*2
     ;;
   "-") 
-    let val=$current-$step
+    let val=$current/2
     ;;
   *)
     exit
