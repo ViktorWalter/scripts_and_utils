907f5bfdba01831b32e14a62bd0db1aa2a9f9c4c
Viktor Walter <viktor_walter@outlook.com>
2021-03-09 17:01:36 +0100

    Updating config for Noetic font issues

--- doti3/install.sh
+++ doti3/install.sh
@@ -34,6 +34,7 @@ sudo gem install rake ncurses-ruby sup gpgme optimist
 wget https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb
 sudo dpkg -i logmein-hamachi_2.1.0.203-1_amd64.deb
 
+sudo apt install fonts-terminus
 cd ~/git
 cd bitmap-fonts
 git clone https://github.com/Tecate/bitmap-fonts.git
