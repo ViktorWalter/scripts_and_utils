a446a436fca79bb1a64bcbe4e376df614bb7f340
Viktor Walter <viktor_walter@outlook.com>
2021-03-09 18:52:59 +0100

    Moving install script to be applicable immediately

--- install.sh
+++ install.sh
@@ -1,125 +1,139 @@
-#!/bin/bash
 
-# get the path to this script
-MY_PATH=`dirname "$0"`
-MY_PATH=`( cd "$MY_PATH" && pwd )`
+toilet "Let's go"
 
-# define paths
-APPCONFIG_PATH=$MY_PATH/appconfig
+#first, replace Tomas' i3 config
 
-cd $MY_PATH
-git submodule update --init --recursive
+rm ~/.i3
+cd `dirname "$0"`
+ln -s `pwd`/doti3 ~/.i3
 
-# install packages
-sudo apt-get -y update
-sudo apt-get -y remove vim-*
-sudo apt-get -y install cmake cmake-curses-gui ruby git sl htop git indicator-multiload figlet toilet gem ruby build-essential tree exuberant-ctags libtool automake autoconf autogen libncurses5-dev python3-dev python2.7-dev libc++-dev clang-3.8 openssh-server pandoc xclip xsel python-git
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+ubuntu-report -f send no
+sudo apt remove dunst popularity-contest unattended-upgrades -y
+sudo apt purge ubuntu-report popularity-contest apport whoopsie -y
+sudo killall dunst
+sudo apt install neovim xfce4-notifyd conky-all fatrace iotop libfftw3-dev mpg123 debtree npm gnome-tweak-tool uuid-dev ruby-full rubygems rake krita inkscape python3-tk golang mesa-utils unclutter mpv lxpolkit tlp \
+libgtk-3-dev \
+libmirclient-dev \
+libxkbcommon-dev \
+libegl1-mesa-dev \
+libwayland-dev \
+libpulse-dev \
+pulseaudio \
+libnotify-dev \
+scrot \
+i3lock \
+w3m \
+w3m-img \
+offlineimap \
+msmtp \
+neofetch \
+gstreamer1.0-{plugins-good,plugins-bad,plugins-ugly,libav} \
+firefox-geckodriver \
+html2text \
+deluge \
+audacity \
+-y
 
-# for mounting exfat
-sudo apt-get -y install exfat-fuse exfat-utils
+sudo pip3 install webdriver selenium
 
-# download, compile and install tmux
-bash $APPCONFIG_PATH/tmux/install.sh
+sudo apt install --install-recommends winehq-staging
+sudo gem install rake ncurses-ruby sup gpgme optimist
+# sudo dpkg -r --force-depends gnome-icon-theme
 
-# compile and install tmuxinator
-bash $APPCONFIG_PATH/tmuxinator/install.sh
+wget https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb
+sudo dpkg -i logmein-hamachi_2.1.0.203-1_amd64.deb
 
-# copy vim settings
-bash $APPCONFIG_PATH/vim/install.sh
+sudo apt install fonts-terminus
+cd ~/git
+cd bitmap-fonts
+git clone https://github.com/Tecate/bitmap-fonts.git
+sudo cp -avr bitmap/ /usr/share/fonts
+xset fp+ /usr/share/fonts/bitmap
+fc-cache -fv
+cd /etc/fonts/conf.d
+sudo rm 70-no-bitmaps.conf && sudo ln -s ../conf.avail/70-yes-bitmaps.conf .
+sudo dpkg-reconfigure fontconfig
+cd ~/
 
-# # install i3
-# bash $APPCONFIG_PATH/i3/install.sh
+cd ~/git
+git clone https://github.com/ironss/pa-applet.git
+cd pa-applet
+./autogen.sh
+./configure
+make
+sudo make install
 
-# setup latex
-bash $APPCONFIG_PATH/vimtex/install.sh
+cd ~/git
+git clone https://github.com/valr/cbatticon.git
+cd cbatticon
+make
+sudo make install
 
-# setup ranger
-bash $APPCONFIG_PATH/ranger/install.sh
+mkdir ~/localpath 
 
-# install athame
-bash $APPCONFIG_PATH/athame/install.sh
+systemctl --user start xfce4-notifyd
+ln -s ~/.i3/dotzsrhc ~/.zshrc
+ln -s ~/.i3/dotsup ~/.sup
+# ln -s ~/.i3/dotXresoures ~/.Xresources
+ln -s ~/.i3/dotXkeymap ~/.Xkeymap
+ln -s ~/.i3/dotofflineimaprc ~/.offlineimaprc
+ln -s ~/.i3/dotofflineimap.py ~/.offlineimap.py
+ln -s ~/.i3/dotmsmtprc ~/.msmtprc
+ln -s ~/.i3/dotsup ~/.sup
+ln -s ~/.i3/dotXkeymap ~/.Xkeymap
+ln -s ~/git/my-linux-setup/linux_setup/dottmux.conf ~/.my.tmux.conf
+sudo ln -s ~/git/networkmanager-dmenu/networkmanager_dmenu /usr/bin/
+ln -s ~/.i3/nmdmenuconfig.ini ~/.config/networkmanager-dmenu/config.ini
 
-#############################################
-# adding GIT_PATH variable to .bashrc 
-#############################################
+~/.i3/install/theme.sh
 
-# add variable for path to the git repository
-num=`cat ~/.bashrc | grep "GIT_PATH" | wc -l`
-if [ "$num" -lt "1" ]; then
+ln -s ~/.i3/dump_site.sh ~/localpath/
+ln -s ~/.i3/windows ~/localpath/
+ln -s ~/.i3/playMusic.sh ~/localpath/
+ln -s ~/.i3/playStudyMusic.sh ~/localpath/
+ln -s ~/.i3/playSong.sh ~/localpath/
+ln -s ~/.i3/imdisp.sh ~/localpath/
+ln -s ~/.i3/reinstpak.sh ~/localpath/
+ln -s ~/.i3/gksudo ~/localpath/
+ln -s ~/.i3/deptree.sh ~/localpath/
+ln -s ~/.i3/ssh-add-identity.sh ~/localpath/
+ln -s ~/.i3/flashMount.sh ~/localpath/
+ln -s ~/.i3/flashUmount.sh ~/localpath/
+sudo mkdir /mnt/flash
 
-  TEMP=`( cd "$MY_PATH/../" && pwd )`
+mkdir ~/.config/networkmanager-dmenu
+touch ~/.myzshrc
+# vim -e  .vimrc <<-SCRIPT
+# g/airline/s/^/"
+# g/tmuxline/s/^/"
+# g/tmuxline/normal!o Plug 'itchyny/lightline.vim'
+# /airline/normal!o Plug 'itchyny/lightline.vim'
+# x
+# SCRIPT
 
-  echo "Adding GIT_PATH variable to .bashrc"
-  # set bashrc
-  echo "
-# path to the git root
-export GIT_PATH=$TEMP" >> ~/.bashrc
+sudo touch /etc/lightdm/lightdm.conf
+sudo echo "
+[SeatDefaults]
+autologin-user=viktor
+autologin-user-timeout=0
+user-session=ubuntu
+# Uncomment the following, if running Unity
+#greeter-session=unity-greeter
+" > /etc/lightdm/lightdm.conf
 
-fi
+echo "export GOPATH=~/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> ~/.zshrc
+go get -u github.com/erroneousboat/slack-term ; cd $GOPATH/src/github.com/erroneousboat/slack-term ; go install .
+ln -s ~/.i3/dotslackterm ~/.slack-term
 
-#############################################
-# add tmux sourcing of dotbashrd to .bashrc
-#############################################
 
-num=`cat ~/.bashrc | grep "RUN_TMUX" | wc -l`
-if [ "$num" -lt "1" ]; then
+sudo cp ~/.i3/thinkpad_power_management/15-brightness.conf /etc/sudoers.d/
 
-  while true; do
-    [[ -t 0 ]] && { read -t 10 -n 2 -p $'\033[31mDo you want to run TMUX automatically with every terminal? [y/n] \033[00m' resp || resp="y" ; }
-    response=`echo $resp | sed -r 's/(.*)$/\1=/'`
 
-    if [[ $response =~ ^(y|Y)=$ ]]
-    then
+~/.i3/install/pa_applet.sh
+~/.i3/install/cbatticon.sh
+~/.i3/install/nordvpn.sh
 
-    echo "
-# want to run tmux automatically with new terminal?
-export RUN_TMUX=true" >> ~/.bashrc
-  
-    echo "Setting variable RUN_TMUX to true"
-
-      break
-    elif [[ $response =~ ^(n|N)=$ ]]
-    then
-
-    echo "
-# want to run tmux automatically with new terminal?
-export RUN_TMUX=false" >> ~/.bashrc
-  
-    echo "Setting variable RUN_TMUX to false"
-
-      break
-    else
-      echo " What? \"$resp\" is not a correct answer. Try y+Enter."
-    fi
-  done
-fi
-
-#############################################
-# creating .vimpath file 
-#############################################
-
-# path for ctags
-# path for file search
-
-#############################################
-# add sourcing of dotbashrd to .bashrc
-#############################################
-num=`cat ~/.bashrc | grep "dotbashrc" | wc -l`
-if [ "$num" -lt "1" ]; then
-
-  echo "Adding source to .bashrc"
-  # set bashrc
-  echo "
-# sourcing tomas's tmux preparation
-source $APPCONFIG_PATH/bash/dotbashrc" >> ~/.bashrc
-
-else
-
-  echo "Reference in .bashrc is already there..."
-
-fi
-
-toilet All Done
-
-# source .bashrc
-bash ~/.bashrc
