093ab91687617e32544b93d13f29b8ea48ae6890
Viktor Walter <viktor_walter@outlook.com>
2021-03-10 14:20:59 +0100

    Fixed the ssh-add-identity script for compatibility with Python3

--- doti3/ssh-add-identity.sh
+++ doti3/ssh-add-identity.sh
@@ -3,6 +3,6 @@
 /usr/bin/ssh-add -l > /dev/null
 if [ $? -ne 0 ]; then
   eval "$(ssh-agent)"
-  password=`python -c "import keyring,sys; sys.stdout.write(keyring.get_password('git-ssh-key','personal'))"`
+  password=`python3 -c "import keyring,sys; print(keyring.get_password('git-ssh-key','personal'))"`
   expect -c " spawn ssh-add; expect \"Enter passphrase for \"; send \"$password\r\"; expect \"%\"; return 0"
 fi
