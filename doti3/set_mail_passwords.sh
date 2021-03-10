09f92f933389f0839ea82f75fbd9c5da6cd40f5c
Viktor Walter <viktor_walter@outlook.com>
2021-03-10 15:07:15 +0100

    Updating the set_mail_passwords.sh script based on destkop

--- doti3/set_mail_passwords.sh
+++ doti3/set_mail_passwords.sh
@@ -2,7 +2,7 @@ echo "GMail password: "
 read gmail_password
 python3 -c "import keyring; keyring.set_password('gmail', 'personal', '${gmail_password}')"
 echo "Outlook password: "
-read gmail_password
+read outlook_password
 python3 -c "import keyring; keyring.set_password('outlook', 'personal', '${outlook_password}')"
 echo "FEL password: "
 read fel_password
