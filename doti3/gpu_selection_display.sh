f177a10f1498a0af17773b01328d676da52db495
Viktor Walter <viktor_walter@outlook.com>
2021-04-01 17:46:37 +0400

    Updating the prime-select display script

--- doti3/gpu_selection_display.sh
+++ doti3/gpu_selection_display.sh
@@ -3,5 +3,9 @@
 
 CURRENT_DEVICE=$(prime-select query | awk '{printf "%s",toupper(substr($1,1,1))tolower(substr($1,2,1))}') 
 
-echo $CURRENT_DEVICE
-if [ "$CURRENT_DEVICE" != "Nv" ]; then exit 33; fi
+if [ "$CURRENT_DEVICE" == "In" ] || [ "$CURRENT_DEVICE" == "Nv" ]; then echo $CURRENT_DEVICE; 
+elif [ "$CURRENT_DEVICE" == "On" ]; then  echo "OD";
+fi
+
+if [ "$CURRENT_DEVICE" != "Nv" ] && [ "$CURRENT_DEVICE" != "On" ];
+then exit 33; fi
