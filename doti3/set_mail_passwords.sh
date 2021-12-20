6636dc3a1eaa9a7b31f6ddf34cdd1ee195227594
Viktor Walter <viktor_walter@outlook.com>
2021-12-20 15:27:17 +0100

    Various updates

--- doti3/set_mail_passwords.sh
+++ doti3/set_mail_passwords.sh
@@ -1,9 +1,12 @@
 echo "GMail password: "
-read gmail_password
+read -s gmail_password
 python3 -c "import keyring; keyring.set_password('gmail', 'personal', '${gmail_password}')"
 echo "Outlook password: "
-read outlook_password
+read -s outlook_password
 python3 -c "import keyring; keyring.set_password('outlook', 'personal', '${outlook_password}')"
 echo "FEL password: "
-read fel_password
+read -s fel_password
 python3 -c "import keyring; keyring.set_password('fel', 'personal', '${fel_password}')"
+echo "Disroot password: "
+read -s disroot_password
+python3 -c "import keyring; keyring.set_password('disroot', 'personal', '${disroot_password}')"
