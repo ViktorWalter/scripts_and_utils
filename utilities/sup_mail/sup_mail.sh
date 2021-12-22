7f4b4a20a318765ffe539adf18759455a3d650ec
Viktor Walter <viktor.walter@disroot.com>
2021-12-22 20:32:35 +0100

    Updating install script for sup mail

--- utilities/sup_mail/sup_mail.sh
+++ utilities/sup_mail/sup_mail.sh
@@ -5,16 +5,16 @@ script_path=`dirname "$0"`
 sudo apt install ruby-full rubygems rake
 sudo gem install rake ncurses-ruby sup gpgme optimist uri -v 0.10.0
 sudo gem uninstall -v 0.10.1
-ln -s ${script_path}/sup_mail/dotsup ~/.sup
+ln -s ${script_path}/dotsup ~/.sup
 
-~/.i3/install/offlineimap3.sh
-ln -s ${script_path}/sup_mail/offlineimap/dotofflineimaprc ~/.offlineimaprc
-ln -s ${script_path}/sup_mail/offlineimap/dotofflineimap.py ~/.offlineimap.py
+${script_path}/offlineimap3.sh
+ln -s ${script_path}/offlineimap/dotofflineimaprc ~/.offlineimaprc
+ln -s ${script_path}/offlineimap/dotofflineimap.py ~/.offlineimap.py
 
 sudo apt install msmtp
-ln -s ${script_path}/sup_mail/msmtp/dotmsmtprc ~/.msmtprc
+ln -s ${script_path}/msmtp/dotmsmtprc ~/.msmtprc
 sudo ln -s /etc/apparmor.d/usr.bin.msmtp /etc/apparmor.d/disable/
 sudo apparmor_parser -R /etc/apparmor.d/usr.bin.msmtp 
 
 sudo apt install lbdb libnet-ldap-perl
-ln -s ${script_path}/sup_mail/lbdb/dotlbdb ~/.lbdb
+ln -s ${script_path}/lbdb/dotlbdb ~/.lbdb
