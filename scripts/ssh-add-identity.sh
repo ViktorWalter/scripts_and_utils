738be2edb68ded082202a877a95680000b16d7ef
Viktor Walter <viktor_walter@outlook.com>
2022-01-25 23:50:51 +0100

    Adding script for installing pass

--- scripts/ssh-add-identity.sh
+++ scripts/ssh-add-identity.sh
@@ -3,6 +3,6 @@
 /usr/bin/ssh-add -l > /dev/null
 if [ $? -ne 0 ]; then
   eval "$(ssh-agent)"
-  password=`python3 -c "import keyring,sys; print(keyring.get_password('git-ssh-key','personal'))"`
+  password=`pass Git/viktor_walter@outlook.com`
   expect -c " spawn ssh-add; expect \"Enter passphrase for \"; send \"$password\r\"; expect \"%\"; return 0"
 fi
