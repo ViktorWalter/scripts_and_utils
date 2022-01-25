974f9329a32508f893fbe9b796ef8b5407407edd
Viktor Walter <viktor_walter@outlook.com>
2022-01-25 22:49:25 +0100

    Updating the script for syncing passwords between machines

--- scripts/sync_password.sh
+++ scripts/sync_password.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-rsync ~/.password-store $1:~/.password-store
+if [ $# -lt 1 ]; then
+  echo "Please supply the hostname of the target machine"
+  exit 1
+fi
+rsync -r ~/.password-store $1:~/.password-store
