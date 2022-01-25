5e47b14297f204201a7f1a3564b0ee4261ba38bf
Viktor Walter <viktor_walter@outlook.com>
2022-01-25 23:48:55 +0100

    Updating the email password setter

--- utilities/sup_mail/set_mail_passwords.sh
+++ utilities/sup_mail/set_mail_passwords.sh
@@ -1,12 +1,12 @@
-echo "GMail password: "
-read -s gmail_password
+#!/bin/bash
+gmail_password=`pass Email/gmail/password`
 python3 -c "import keyring; keyring.set_password('gmail', 'personal', '${gmail_password}')"
-echo "Outlook password: "
-read -s outlook_password
+
+outlook_password=`pass Email/outlook/password`
 python3 -c "import keyring; keyring.set_password('outlook', 'personal', '${outlook_password}')"
-echo "FEL password: "
-read -s fel_password
+
+fel_password=`pass Email/fel/password`
 python3 -c "import keyring; keyring.set_password('fel', 'personal', '${fel_password}')"
-echo "Disroot password: "
-read -s disroot_password
+
+disroot_password=`pass Email/disroot/password`
 python3 -c "import keyring; keyring.set_password('disroot', 'personal', '${disroot_password}')"
