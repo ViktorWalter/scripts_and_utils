8f3998541707b5112dd4ec694be72997ff2be694
Viktor Walter <viktor_walter@outlook.com>
2022-01-26 13:19:48 +0100

    Updating the mail password setting script

--- utilities/sup_mail/set_mail_passwords.sh
+++ utilities/sup_mail/set_mail_passwords.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-gmail_password=`pass Email/gmail/password`
+gmail_password=`pass Email/gmail | head -n1`
 python3 -c "import keyring; keyring.set_password('gmail', 'personal', '${gmail_password}')"
 
-outlook_password=`pass Email/outlook/password`
+outlook_password=`pass Email/outlook | head -n1`
 python3 -c "import keyring; keyring.set_password('outlook', 'personal', '${outlook_password}')"
 
-fel_password=`pass Email/fel/password`
+fel_password=`pass Email/fel | head -n1`
 python3 -c "import keyring; keyring.set_password('fel', 'personal', '${fel_password}')"
 
-disroot_password=`pass Email/disroot/password`
+disroot_password=`pass Email/disroot | head -n1`
 python3 -c "import keyring; keyring.set_password('disroot', 'personal', '${disroot_password}')"
