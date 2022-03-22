1d63796d9a3c66bccb77de0b8ba47feb22c71e81
Viktor Walter <viktor.walter@disroot.com>
2022-03-22 14:50:23 +0100

    Updating the git autologin script

--- scripts/git_autologin.sh
+++ scripts/git_autologin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 argins="$@"
-command="git ${argins}"
+command="env GIT_MERGE_AUTOEDIT=no GIT_EDITOR=true git ${argins}"
 expect -c "spawn $command; expect \"Username for\"; exit" 
 login_prompt=`expect -c "spawn $command; expect \"Username for\"; exit" `
 
@@ -9,10 +9,11 @@ if  echo $login_prompt | grep -qi "overleaf" ; then
   # echo "what"
   login=`pass Work/Overleaf_ORCID | tail -n 1 | cut -d " " -f2`
   password=`pass Work/Overleaf_ORCID | head -n 1`
-  expect -c " spawn $command; expect \"Username for \"; send \"$login\r\"; expect \"Password for \"; send \"$password\r\""
-  exit 0
+## Add more passwords
 else
   echo "I don't know this identity. Please add it to the git_autologin.sh script."
   exit 1
 fi
 
+expect -c " spawn $command; expect \"Username for \"; send \"$login\r\"; expect \"Password for \"; send \"$password\r\"; interact"
+exit 0
