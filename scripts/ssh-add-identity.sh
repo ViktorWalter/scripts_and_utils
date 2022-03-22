8bf322944fce2a57e92a7c17851ad7f4c3e9313a
Viktor Walter <viktor.walter@disroot.com>
2022-03-22 14:17:31 +0100

    Updating the ssh-agent autologin script

--- scripts/ssh-add-identity.sh
+++ scripts/ssh-add-identity.sh
@@ -3,6 +3,6 @@
 /usr/bin/ssh-add -l > /dev/null
 if [ $? -ne 0 ]; then
   eval "$(ssh-agent)"
-  password=`pass Git/viktor_walter@outlook.com`
+  password=`pass Work/Git_1 | head -n 1`
   expect -c " spawn ssh-add; expect \"Enter passphrase for \"; send \"$password\r\"; expect \"%\"; return 0"
 fi
