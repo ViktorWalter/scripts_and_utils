d91d9da92d76455decb759f5f1052469bc11935e
Viktor Walter <viktor.walter@disroot.com>
2022-04-07 16:22:59 +0200

    Removing automatic login to Git

--- scripts/ssh-add-identity.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin/zsh
-# /usr/bin/ssh-add -l
-/usr/bin/ssh-add -l > /dev/null
-if [ $? -ne 0 ]; then
-  eval "$(ssh-agent)"
-  password=`pass Work/Git_1 | head -n 1`
-  expect -c " spawn ssh-add; expect \"Enter passphrase for \"; send \"$password\r\"; expect \"%\"; return 0"
-fi
