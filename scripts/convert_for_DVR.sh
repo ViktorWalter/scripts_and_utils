89c8c2999bd400ddbee48dfc76d16359fdfa8ef0
Viktor Walter <viktor_walter@outlook.com>
2022-09-20 14:55:08 +0200

    Updating a video conversion script and msmtp settings

--- scripts/convert_for_DVR.sh
+++ scripts/convert_for_DVR.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 file=$1
-ffmpeg -i "$file" -vcodec dnxhd -acodec pcm_s16le -s 1920x1080 -r 30000/1001 -b:v 36M -pix_fmt yuv422p -f mov "${file%.mp4}_c.mov"
+# ffmpeg -i "$file" -vcodec dnxhd -profile:v dnxhr_hqx -acodec pcm_s16le -s 4096x2160 -r 60000/1001 -b:v 36M -pix_fmt yuv422p10le -f mov "${file%.mp4}_c.mov"
+ffmpeg -i "$file" -vcodec dnxhd -profile:v dnxhr_hqx -acodec pcm_s16le -s 4096x2160 -b:v 36M -pix_fmt yuv422p10le -f mov "${file%.mp4}_c.mov"
